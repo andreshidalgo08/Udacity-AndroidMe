77a57a84d5e1a58460a157a2158ece0d36a5b360
Andres Hidalgo <andreshidalgo08@gmail.com>
2018-09-30 19:59:57 -0500

    Did todos

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
